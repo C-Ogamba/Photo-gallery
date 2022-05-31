231e8b76af64e1139e02db0274d8b4bac0021b07
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-05-31 20:09:56 +0300

    Feat: debugging

--- photoshare/asgi.py
+++ photoshare/asgi.py
@@ -8,12 +8,14 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
-
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..' )
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../photoshare')
 
 
 from django.core.asgi import get_asgi_application
 
-# os.environ['DJANGO_SETTINGS_MODULE'] = 'photo-gallery.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'photoshare.settings')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'photoshare.settings'
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'photo-gallery.settings')
 
 application = get_asgi_application()
