04999a9db8e9f088eb691da1ffd0d886b838794b
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-05-31 18:36:34 +0300

    deployment errors

--- photoshare/wsgi.py
+++ photoshare/wsgi.py
@@ -11,11 +11,11 @@ import os
 import sys
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..' )
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../project')
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../photo-gallery')
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'project.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'photo-gallery.settings'
 
 # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'photo-gallery.settings')
 
